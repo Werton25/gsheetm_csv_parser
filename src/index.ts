7c5580dc381affd5fbd5f0a76e2c1d9ad9ca80bf
Andrei Kuharev <andrei0495@gmail.com>
2018-06-26 00:10:08 +0300

    Read csv file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
+import csv from 'fast-csv';
+import { Stream } from 'stream';
 
+class Runner {
+    private stream: Stream;
+
+    constructor() {
+        this.stream = new Stream();
+    }
+
+    run(): void {
+        const csvStream = csv.fromPath('csv/transactions.csv')
+            .on('data', Runner.data);
+
+        this.stream.pipe(csvStream);
+    }
+
+    private static data(data: string): void {
+        console.log(data);
+    }
+}
+
+(() => {
+    const runner = new Runner();
+    runner.run();
+})();
